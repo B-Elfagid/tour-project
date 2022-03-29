33b25563092f6af0a62cbfc1591380effd03607d
Betty <betty_elfagid@yahoo.co.uk>
2022-03-29 23:37:05 +0100

    set fetching functionality to fetch from url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import Loading from './Loading';
+import Tours from './Tours';
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
 
   const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([])
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true)
+
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+    
+  }
+
+  useEffect(() =>{
+     fetchTours()
+  }, [])
+
 
   if(loading) {
     return (
@@ -14,7 +36,11 @@ function App() {
     )
   }
 
-  return<h2>Tours project excercise</h2>
+  return (
+   <main>
+      <Tours />
+    </main>
+  )
 }
 
 export default App;
